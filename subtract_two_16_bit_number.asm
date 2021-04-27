bb3d40dc10be08b9beb82a1809f7e71dccd0f2f2
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-27 23:57:43 +0530

    Update subtract_two_16_bit_number.asm

--- subtract_two_16_bit_number.asm
+++ subtract_two_16_bit_number.asm
@@ -1,12 +1,12 @@
-LHLD 6500 ; Load 1st 16 bit in HL Pair with address 6500
+LHLD 6500H ; Load 1st 16 bit in HL Pair with address 6500
 XCHG ; Exchange DE and HL Pair
-LHLD 6502	; Load 2nd 16 bit in HL Pair with address 6502
+LHLD 6502H	; Load 2nd 16 bit in HL Pair with address 6502
 MOV A, E ; Move data from Register E to Accumulator
 SUB L ; A = A-L
-STA 6054 ; Store the subtraction result of lower operand to memory location 6504 from Accumulator
+STA 6054H ; Store the subtraction result of lower operand to memory location 6504 from Accumulator
 MOV L,A
 MOV A, D ; Move value from Register D to Accumulator
 SBB H ; Subtract with Borrow the Higher Bits
 MOV H,A
-SHLD 6505 ;  Store the subtraction result of higher operand to memory location 6505 from Accumulator
+SHLD 6505H ;  Store the subtraction result of higher operand to memory location 6505 from Accumulator
 HLT
