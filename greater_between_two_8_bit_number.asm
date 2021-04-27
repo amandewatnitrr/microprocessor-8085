79cec2a6ec229219a80bf7a6f5dfc9c1988b4b11
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-27 23:52:58 +0530

    Update greater_between_two_8_bit_number.asm

--- greater_between_two_8_bit_number.asm
+++ greater_between_two_8_bit_number.asm
@@ -8,3 +8,14 @@ STA 0000 ;  Now we will store the largest number in memory location 0000
 LOOP: MOV A,C ; Now, this will get executed, now the smaller number is transfered to Accumulator
       STA 0001 ; From the accumulator the smaller number is than stored at 0001 Memory location.                                        
       HLT
+
+/*Another Method*/
+LXI H,0001H ; Extract both the 2 8-bit numbers from memory location 0001 and load to HL Pair
+MOV A,M ; Move the first number to the Accumulator
+INX H ;  increment the HL Pointer to point to the next memory location
+CMP M ; Compare the value in Accumulator with the value at the location wehre currently HL Pointer points
+JNC UP ; If no carry is generated, the Program counter jumps to UP and increments the HL Pointer to next location and stores the value in Accumulator over there, as it's the largest.
+MOV A,M ; if Carry is generated, this mean the number pointed by HL Pointer is larger compared to that in Accumulator and hence we need to transfer it to Accumulator
+UP: INX H ; Once the larger number is transfered to Accumulator, increment the HL Pointer to next memory location and store the largest number over there.
+STA 0003H
+HLT
