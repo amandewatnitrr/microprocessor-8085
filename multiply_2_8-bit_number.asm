1ee05f76d355052cf55e149daec91cfd66c2d6b6
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-28 04:20:52 +0530

    Update multiply_2_8-bit_number.asm

--- multiply_2_8-bit_number.asm
+++ multiply_2_8-bit_number.asm
@@ -1,7 +1,7 @@
-LHLD 6501 ; Load the Multiplicand in HL Pair from the specified memory Address
+LHLD 6501H ; Load the Multiplicand in HL Pair from the specified memory Address
 XCHG ; exchange the content of HL Pair with DE
 LDA 6503H ; Load the other number stored in 6503H location to the Accumulator
-LXI H,0000 ; Set initial product in HL Pair to 00
+LXI H,0000H ; Set initial product in HL Pair to 00
 MVI C,08 ; Count=08 in Register C
 up:DAD H ; Shift partial Product left by 1-bit
 RAL ; Rotate Multiplier by 1-bit. is multiplier = 1 ?
@@ -9,5 +9,5 @@ JNC down ; If not, than we go ahead
 DAD D ; Peroduct = Product + Multiplicand
 down:DCR C ; Decrement Count
 JNZ up ; Jump untill C=0
-SHLD 6504 ; Store result in 6504
+SHLD 6504H ; Store result in 6504
 HLT ; END
