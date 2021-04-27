87b1099eba1815b95710c989d19c3d74cbd5aace
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-27 13:09:00 +0530

    Update add_two_8_bit_number.asm

--- add_two_8_bit_number.asm
+++ add_two_8_bit_number.asm
@@ -1,4 +1,16 @@
-MVI A,14H
-MVI B,86H
-ADD B
-HLT
+The addition here is of HexaDecimal numbers.
+
+/*1st Way*/
+MVI A,14H ; Transfers Hexadecimal Data 14H to Accumulator
+MVI B,86H ; Transfers Hexadecimal Data 86H to register B
+ADD B ; Add the data in Register B to Data in Accumulator
+HLT ; End of Program
+
+/*2nd Way*/
+LXI H,7500H ; Get the number stored at Memory Address  7500H. This loads the data to Memory
+MOV A,M ; Now move the number in Memory to Accumulator
+INX H ; Increment H to point to next memory location, that contains the other number and now this number is transfered to the memory.
+ADD M ; Now Add the value in Accumulator to the number in Memory or the number pointed by the pointer.
+INX H ; Again increment the pointer to the next location here we will save our output
+MOV M,A ; Move the result from the memory to the Accumulator
+HLT ; End of program
